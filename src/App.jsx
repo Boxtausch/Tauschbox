068ba648daeab62c739636cd77976694a64f5c5c
Boxtausch <mirco.meier@bluewin.ch>
2026-05-23 20:39:38 +0200

    Create App component with initial layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div style={{
+      fontFamily: "Arial",
+      padding: 40,
+      textAlign: "center"
+    }}>
+      <h1>TauschBox 🚀</h1>
+      <p>Tauschen statt wegwerfen 🌱</p>
+    </div>
+  )
+}
